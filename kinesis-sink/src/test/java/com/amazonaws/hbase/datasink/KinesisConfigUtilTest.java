270a58f4eeeee44e73d782b95e07cf6c62e56b7c
shenavaa <shenavandeh@gmail.com>
2021-10-11 17:40:50 +1100

    default region

--- kinesis-sink/src/test/java/com/amazonaws/hbase/datasink/KinesisConfigUtilTest.java
+++ kinesis-sink/src/test/java/com/amazonaws/hbase/datasink/KinesisConfigUtilTest.java
@@ -10,9 +10,14 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.SdkClientException;
 import com.amazonaws.regions.Regions;
 
 public class KinesisConfigUtilTest {
+	private static final Logger LOG = LoggerFactory.getLogger(KinesisConfigUtilTest.class);
 
 	private Configuration config;
 	@Before
@@ -36,13 +41,19 @@ public class KinesisConfigUtilTest {
 	
 	@Test
 	public void testRegion() throws Exception {
-		String region = Regions.getCurrentRegion().getName();
-
-		config.set("hbase.replication.kinesis.region","ap-southeast-2");
+		String region="dummy-region";
+		try {
+			region = Regions.getCurrentRegion().getName();
+		} catch (SdkClientException e) {
+			// not running on AWS
+			LOG.info("Test not running on EC2. setting region to: " + region);
+			
+		}
+		config.set("hbase.replication.kinesis.region",region);
 		KinesisConfigurationUtil util = new KinesisConfigurationUtil(config);
 		
 		System.out.println("Default Region: " + util.getKPLConfiguration().getRegion() + " Region: " + region);
-		assertEquals("ap-southeast-2",util.getKPLConfiguration().getRegion());
+		assertEquals(region,util.getKPLConfiguration().getRegion());
 	}
 	
 	@Test
